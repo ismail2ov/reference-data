0c5fad2d7216f41d5983b064e1de82bf389d68c0
Ismail Ahmedov <ismail2ov@gmail.com>
2025-09-05 17:25:17 +0200

    spotless apply

--- reference-data-api/src/test/java/com/sixgroup/reference_data_api/ReferenceDataApiApplicationTests.java
+++ reference-data-api/src/test/java/com/sixgroup/reference_data_api/ReferenceDataApiApplicationTests.java
@@ -1,15 +1,16 @@
 package com.sixgroup.reference_data_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+
+import org.junit.jupiter.api.Test;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class ReferenceDataApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- reference-data-api/src/test/java/com/sixgroup/reference_data_api/TestReferenceDataApiApplication.java
+++ reference-data-api/src/test/java/com/sixgroup/reference_data_api/TestReferenceDataApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 
 public class TestReferenceDataApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(ReferenceDataApiApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.from(ReferenceDataApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
 
 }
